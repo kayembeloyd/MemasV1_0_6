9a9e9da155d3e2ff29a9088a3c7f5e0f96c80de1
Lloyd Kayembe <kayembeloyd@gmail.com>
2022-11-01 17:54:52 +0200

    installation of the camera

--- memas/screens/ScanScreen.jsx
+++ memas/screens/ScanScreen.jsx
@@ -1,20 +1,38 @@
 import React, { useState, useEffect } from 'react'
 import { Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function ScanScreen({ navigation }){
     const [type, setType] = useState(CameraType.back);
     const [permission, requestPermission] = Camera.useCameraPermissions();
 
+    const [hasPermission, setHasPermission] = useState(null); 
+
     useEffect(() => {
+        const getBarCodeScannerPermissions = async () => {
+            const { status } = await BarCodeScanner.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        };
+    
+        getBarCodeScannerPermissions();
+
         const unsubscribe = navigation.addListener('focus', () => {
             console.log('Scan Screen Opened'); 
             console.log('checking camera permissions')
             requestPermission()
         });
-
+        
         return unsubscribe;
     }, [ navigation ]);
+
+    if (hasPermission === null) {
+        return <Text>Requesting for camera permission</Text>;
+    }
+
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
 
     function toggleCameraType() {
         setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
@@ -23,34 +41,22 @@ export default function ScanScreen({ navigation }){
 
     return (
         <View style={styles.container}>
-            {Platform.OS === 'web' ? 
-                <Camera style={{height: '100%'}} type={type}>
-                    <Text style={{color: 'white'}}>Scan Screen</Text>
-                    <Text style={{color: 'white'}}>Platform = { Platform.OS }</Text>
-                    <TouchableOpacity onPress={toggleCameraType}>
-                        <Text style={{color: 'white'}}>Flip Camera</Text>
-                    </TouchableOpacity>
-                </Camera>
-
-                : 
-                
-                <Camera style={{height: '100%'}} type={type}>
-                    <Text style={{color: 'white'}}>Scan Screen</Text>
-                    <Text style={{color: 'white'}}>Platform = { Platform.OS }</Text>
-                    <TouchableOpacity onPress={toggleCameraType}>
-                        <Text style={{color: 'white'}}>Flip Camera</Text>
-                    </TouchableOpacity>
-                </Camera>    
-            }
-
-
-            
+            <Camera 
+                style={{height: '100%', width: '100%'}} 
+                type={type} 
+                barCodeScannerSettings={{ barCodeTypes: [BarCodeScanner.Constants.BarCodeType.qr], }}
+                onBarCodeScanned = {(d) => {
+                    console.log("Barcode scanned")
+                    console.log("d: ", d)
+               }}>
+            </Camera> 
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        padding: 40,
+        flex: 1,
+        backgroundColor: 'red',
     }
 })
